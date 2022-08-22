2c3a6af4052c024646e3122234a161185c07e4c2
Ram Prasann Pandey <106461823+Ramprasann@users.noreply.github.com>
2022-08-22 12:50:18 +0530

    do while program

---
+++ dowhile.java
@@ -0,0 +1,28 @@
+// public class dowhile {
+//     public  static void main(String args[]) {
+//         int c=1;
+//         do{
+//             System.out.println(c);
+//             c++;
+//             break;
+    
+//         }while(c<=10);
+        
+        
+//     }
+    
+// }
+public class dowhile {
+    public  static void main(String args[]) {
+        int c=1;
+        do{
+            System.out.println(c);
+            c++;
+            break;
+    
+        }while(c<=10);
+        
+        
+    }
+    
+}
