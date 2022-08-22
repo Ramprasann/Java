6c35bb0a38392da716a6a5fa08d88b158bfcb110
Ram Prasann Pandey <106461823+Ramprasann@users.noreply.github.com>
2022-08-22 12:36:34 +0530

    binary operator

---
+++ binaryope.java
@@ -0,0 +1,21 @@
+public class binaryope {
+     public static void main(String args[]) {
+       int  a=10,b=5;
+       int c=a+b;
+        int d=a-b;
+        int e=a*b;
+        int f=a/b;
+        int g=a%b;
+        System.out.print(c+"\n");
+        System.out.print(d+"\n");
+        System.out.print(e+"\n");
+        System.out.print(f+"\n");
+        System.out.print(g+"\n");
+        
+
+
+
+        
+    }
+    
+}
