8a0bc98b322d57f7f9486a0c4556a3f87a76af99
Ram Prasann Pandey <106461823+Ramprasann@users.noreply.github.com>
2022-08-22 12:37:19 +0530

    circle

---
+++ circle.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class circle {
+
+    public static void main(String args[]) {
+    
+        Scanner ram = new Scanner(System.in);
+            float r = ram.nextFloat();
+         Float c = 3.14f * r * r;
+          //  c = (float) (3.14 * r * r);
+        
+        System.out.println(c);
+        ram.close();
+        
+    }
+
+}
