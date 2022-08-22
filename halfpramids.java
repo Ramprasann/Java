e50cbfaa0a549f526e5ab7e0bfc2fe35cd5af4bd
Ram Prasann Pandey <106461823+Ramprasann@users.noreply.github.com>
2022-08-22 12:52:13 +0530

    half pyramids

---
+++ halfpramids.java
@@ -0,0 +1,13 @@
+public class halfpramids {
+    public static void main(String args[]){
+        int i=1,j;
+        for(i=1;i<5;i++){
+            j=1;
+            for(j=1;j<=i;j++)
+            System.out.print(j);
+            System.out.println();
+        }
+
+    }
+    
+}
