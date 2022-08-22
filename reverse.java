fc69cedb304dcadc56009f4aa7ade42d4e6c6d4b
Ram Prasann Pandey <106461823+Ramprasann@users.noreply.github.com>
2022-08-22 12:55:42 +0530

    reverse no

---
+++ reverse.java
@@ -0,0 +1,12 @@
+public class reverse {
+    public static void main(String args[]){
+        int a=10889;
+        while(0<a){
+         int c= a%10;
+         a=a/10;
+         System.out.print(c);
+    }
+    System.out.println("hello");
+    }
+    
+}
